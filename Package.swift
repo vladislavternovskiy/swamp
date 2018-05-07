ae8de1c51329f7a0c83f6f52e5d84be2ee21876d
L, Oleg <oleg.l@mail.int>
2018-05-07 16:31:18 +0300

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
 		)
 	],
 	dependencies: [
-        .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", versions: Version(1,0,0)..<Version(3, .max, .max)),
+        .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", versions: Version(1,0,0)..<Version(4, .max, .max)),
 		.Package(url: "https://github.com/daltoniam/Starscream", majorVersion: 2, minor: 0),
 		.Package(url: "https://github.com/krzyzanowskim/CryptoSwift", majorVersion: 0, minor: 6),
 	],
