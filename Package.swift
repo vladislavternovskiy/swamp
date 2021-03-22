a903077d6864fc4467faea4eaa8d4925b3dbb326
Vladyslav Ternovskyi <vladislav.ternovskii@gmail.com>
2021-03-22 15:24:49 +0200

    update package manager

--- Package.swift
+++ Package.swift
@@ -9,9 +9,8 @@ let package = Package(
 		)
 	],
 	dependencies: [
-        .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", versions: Version(1,0,0)..<Version(3, .max, .max)),
-		.Package(url: "https://github.com/daltoniam/Starscream", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/krzyzanowskim/CryptoSwift", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/daltoniam/Starscream.git", majorVersion: 4)
+		.Package(url: "https://github.com/krzyzanowskim/CryptoSwift", majorVersion: 1),
 	],
 	exclude: ["Example"]
 )
